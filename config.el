7d0ec80e3b0a670ad421344cf74a817d39fbae42
Francois Michaut <fmichaut@upmem.com>
2021-11-02 10:18:48 +0100

    Few fixes

--- config.el
+++ config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Francois Michaut"
+      user-mail-address "fmichaut@upmem.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -54,6 +54,7 @@
 ;; they are implemented.
 (remove-hook 'tty-setup-hook #'xterm-mouse-mode)
 (global-display-fill-column-indicator-mode 1)
+(setq tramp-histfile-override t)
 
 (map! "C-c C-<up>" 'windmove-up)
 (map! "C-c C-<down>" 'windmove-down)
